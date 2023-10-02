5897171f3041726a47c6a0040fbfa218df402e41
Nicollas Cruz <nicollasacruz@gmail.com>
2023-10-02 11:35:57 -0300

    Fix test before route changes
    Signed-off-by: Nicollas Cruz <nicollasacruz@gmail.com>

--- volumes/libresign-e-commerce/routes/web.php
+++ volumes/libresign-e-commerce/routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::view('/', 'welcome')->name('home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
